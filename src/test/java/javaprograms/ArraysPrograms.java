4f61d6f91a3cac18c57fb5d7f9027a2921e76819
singh-surendra <surendrasingh.db@gmail.com>
2020-04-08 22:52:04 +0200

    array program

---
+++ src/test/java/javaprograms/ArraysPrograms.java
@@ -0,0 +1,38 @@
+package javaprograms;
+
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ArraysPrograms {
+
+
+    @Test
+    public void removeZeroAndPlaceThemAtEnd() {
+
+        Integer[] array = {0, 1, 3, 0, 7, 9, 0};
+        List<Integer> list = new ArrayList<>(Arrays.asList(array));
+        int noOfZeros = 0;
+        int origSize = list.size();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == 0) {
+                list.remove(i);
+                noOfZeros++;
+            }
+        }
+        if(noOfZeros>0) {
+            for (int i = noOfZeros + 1; i < origSize; i++) {
+                list.add(i, 0);
+            }
+        }
+        System.out.println(list);
+    }
+
+
+
+
+
+}
---
+++ src/test/java/javaprograms/GettersSetters.java
@@ -0,0 +1,40 @@
+package javaprograms;
+
+public class GettersSetters {
+
+
+    private String breed;
+    private int age;
+    private String color;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+
+
+
+
+}
